4be9a9eb357fd371b88ad3c00a047d6d9915a790
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-12-11 17:22:00 +0600

    Update Payment.tsx

--- src/pages/Payment/Payment.tsx
+++ src/pages/Payment/Payment.tsx
@@ -70,7 +70,7 @@ const Payment = () => {
 
     try {
       const response = await axios.post(
-        'http://127.0.0.1:5000/make_appointment', 
+        'http://happymedkz.serveo.net/make_appointment', 
         appointmentData, 
         { headers: { 'Content-Type': 'application/json' } }
       );
