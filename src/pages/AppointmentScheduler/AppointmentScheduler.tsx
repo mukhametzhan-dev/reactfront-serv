21e174c6af765342a32f7d9ec7b65f155cfed397
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-12-12 01:47:57 +0600

    Update AppointmentScheduler.tsx

--- src/pages/AppointmentScheduler/AppointmentScheduler.tsx
+++ src/pages/AppointmentScheduler/AppointmentScheduler.tsx
@@ -58,7 +58,7 @@ const AppointmentScheduler = ({ doctorEmail }) => {
     try {
       // Submit the appointmentData as JSON
       const response = await axios.post(
-        'https://happymedkz.serveo.net/make_appointment',
+        'https://happymed.duckdns.org/make_appointment',
         appointmentData,
         {
           headers: {
