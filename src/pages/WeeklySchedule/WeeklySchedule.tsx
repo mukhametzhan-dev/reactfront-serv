70d9754db0e8e8302a3f6b806c8730d3bdfeb6cb
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-12-12 02:08:05 +0600

    Update WeeklySchedule.tsx

--- src/pages/WeeklySchedule/WeeklySchedule.tsx
+++ src/pages/WeeklySchedule/WeeklySchedule.tsx
@@ -83,7 +83,7 @@ const WeeklySchedule: React.FC<WeeklyScheduleProps> = ({
   const fetchExistingSchedule = async (email: string) => {
     try {
       const response = await axios.get(
-        `https://happymedkz.serveo.net/get_schedule?email=${email}`
+        `https://happymed.duckdns.org/get_schedule?email=${email}`
       );
       if (response.status === 200 && response.data.schedule) {
         const existingSchedule: ScheduleResponse = response.data;
