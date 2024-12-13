24a6931d01edff5ea981bc4917f4e61c418b835c
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-12-13 09:28:04 +0600

    Update Transactions.tsx

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -26,7 +26,7 @@ const Transactions = () => {
     if (storedUser) {
       try {
         const parsedUser = JSON.parse(storedUser);
-        const response = await axios.get(`http://localhost:5000/get_balance`, {
+        const response = await axios.get(`https://happymed.work.gd/get_balance`, {
           params: { email: parsedUser.email },
         });
         if (response.status === 200) {
@@ -47,7 +47,7 @@ const Transactions = () => {
       try {
         const parsedUser = JSON.parse(storedUser);
 
-        const response = await axios.get(`http://localhost:5000/get_transactions`, {
+        const response = await axios.get(`https://happymed.work.gd/get_transactions`, {
           params: { patient_id: parsedUser.patient_id },
         });
         if (response.status === 200) {
