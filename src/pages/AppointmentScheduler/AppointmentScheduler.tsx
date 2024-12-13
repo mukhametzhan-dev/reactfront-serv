d4e8e532805426a8a4f0dee3e6c5d7a353c2f332
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-12-13 09:22:51 +0600

    Update AppointmentScheduler.tsx

--- src/pages/AppointmentScheduler/AppointmentScheduler.tsx
+++ src/pages/AppointmentScheduler/AppointmentScheduler.tsx
@@ -58,7 +58,7 @@ const AppointmentScheduler = ({ doctorEmail }) => {
     try {
       // Submit the appointmentData as JSON
       const response = await axios.post(
-        'http://127.0.0.1:5000/make_appointment',
+        'https://happymed.work.gd/make_appointment',
         appointmentData,
         {
           headers: {
