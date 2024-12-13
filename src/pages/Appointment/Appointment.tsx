f065d6db511bb5ae7ea4247d1cc836d4e17dc3d4
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-12-13 09:22:30 +0600

    Update Appointment.tsx

--- src/pages/Appointment/Appointment.tsx
+++ src/pages/Appointment/Appointment.tsx
@@ -65,7 +65,7 @@ export const Appointment = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:5000/doctors');
+        const response = await axios.get('https://happymed.work.gd/doctors');
         if (response.status === 200) {
           const fetchedDoctors = response.data;
           setDoctors(fetchedDoctors);
